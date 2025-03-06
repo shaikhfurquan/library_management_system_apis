97a953378009d7a33a08661322b65a6e51687409
shaikhfurquan <shaikhfurquan313@gmail.com>
2025-03-07 00:55:00 +0530

    logout and user profile

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -134,3 +134,26 @@ export const loginUser = catchAsyncError(async (req, res, next) => {
     }
 })
 
+
+export const logoutUser = catchAsyncError(async (req, res, next) => {
+    res
+        .status(200)
+        .cookie('token', "", {
+            expires: new Date(Date.now()),
+            httpOnly: true
+        })
+        .json({
+            success: true,
+            message: "Logged out successfully"
+        });
+})
+
+
+export const getUserProfile = catchAsyncError(async (req, res, next) => {
+    const user = req.user
+    res.status(200)
+        .json({
+            success: true,
+            user
+        });
+})
