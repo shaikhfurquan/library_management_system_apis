265576002be7bd8a5edc1dad89bec1751cd4e1b9
shaikhfurquan <shaikhfurquan313@gmail.com>
2025-03-10 22:24:05 +0530

    borrow model

---
+++ models/borrow.model.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+const borrowSchema = new mongoose.Schema({
+    user: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+        },
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+    },
+    borrowDate: {
+        type: Date,
+        default: Date.now
+    },
+    returnDate: {
+        type: Date,
+        required: true
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    fine: {
+        type: Number,
+        default: 0
+    },
+    notified: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+const BorrowModel = mongoose.model('Borrow', borrowSchema)
+
+export default BorrowModel
